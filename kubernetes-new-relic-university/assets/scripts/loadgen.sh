a3ef132c97dfd865d340140e7ca63aad65a1eaf7
Stijn Polfliet <spolfliet@newrelic.com>
2020-04-29 14:21:37 +0200

    Get frontend ip

--- kubernetes-new-relic-university/assets/scripts/loadgen.sh
+++ kubernetes-new-relic-university/assets/scripts/loadgen.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Specify the frontend service IP, for example: ./loadgen.sh <frontend_svc_ip>"
-    exit
-fi
-
-HOST=$1
 
 while true; do  
-for i in {1..1000}
-do
+    for i in {1..1000}
+    do
+        HOST=`kubectl get svc frontend -n demo-app | grep frontend | awk '{print $3}'`
         curl -d "message=automated_message_$i" -X POST http://$HOST/message 
         sleep $(( ( RANDOM % 10 )  + 1 ))
-done
+    done
 done
