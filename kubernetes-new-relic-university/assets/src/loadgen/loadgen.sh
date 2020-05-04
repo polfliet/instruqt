931b1a6c5d51ff59f9327da64ba50fd28385813d
Samuel Vandamme <samuel@sava.be>
2020-05-04 11:43:46 +0200

    Made load generator send real text messages using fortune

--- kubernetes-new-relic-university/assets/src/loadgen/loadgen.sh
+++ kubernetes-new-relic-university/assets/src/loadgen/loadgen.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-while true; do  
+while true; do
     for i in {1..1000}
     do
         HOST=frontend
-        curl -d "message=automated_message_$i" -X POST http://$HOST/message 
+        MESSAGE=$(fortune | tr -d '\n')
+        echo "Load generator: posting to $HOST message: $MESSAGE"
+        curl --data-urlencode "message=$MESSAGE" -X POST http://$HOST/message
         sleep $(( ( RANDOM % 10 )  + 1 ))
     done
 done
