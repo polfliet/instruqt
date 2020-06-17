88944639584895a9c52448cc39ef2b180af89ca0
Stijn Polfliet <spolfliet@newrelic.com>
2020-06-17 15:34:08 +0200

    Update mongo setup

--- activate-infrastructure/assets/newrelic/setupMongo.js
+++ activate-infrastructure/assets/newrelic/setupMongo.js
@@ -1,1 +1,19 @@
-use admin
+use admin;
+
+db.createRole({
+    role: "listCollections",
+    privileges: [{
+        resource: {db:"",collection:""},
+        actions: ["listCollections"]
+    }],
+    roles: []
+});
+
+db.createUser({
+    user: "newrelic",
+    pwd: "newrelic",
+    roles: [
+        "clusterMonitor",
+        "listCollections"
+    ]
+});
